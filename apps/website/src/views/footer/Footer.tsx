9429de0852cc04fcd0b0fb3fa3a46ebcaff709a3
Doni <vissmadr@pm.me>
2025-10-25 17:52:58 +0300

    update(website): footer

--- apps/website/src/views/footer/Footer.tsx
+++ apps/website/src/views/footer/Footer.tsx
@@ -3,7 +3,15 @@ import css from "./Footer.module.css";
 export const Footer = () => {
   return (
     <footer class={css.footer}>
-      <p>Hand-made</p>
+      <a
+        class={css.link}
+        href={"https://github.com/vissmadr"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <code class={css.code}>handcrafted</code>
+        <code class={css.code}>v1.0</code>
+      </a>
     </footer>
   );
 };
